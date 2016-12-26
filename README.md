b27186b459b295870d57f7f3a1844a5fd46f728e
Elizabeth Linn <elise_81@yahoo.com>
2016-12-26 12:04:15 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+https://app.pluralsight.com/library/courses/npm-build-tool-introduction/table-of-contents
+
+
---
+++ test/test.js
@@ -0,0 +1,7 @@
+var should = require('should');
+
+describe('the most simple test', function () {
+    it('true should be true', function () {
+        true.should.equal(true);
+    });
+});
